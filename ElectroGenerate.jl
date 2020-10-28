5013166bf7789be348e0c44352040249276e7ed5
TPPrescott <tpprescott@gmail.com>
2020-10-28 14:14:31 +0000

    Change defaults - too small synthetic_likelihood_n increases variance in L and long run slows down inference

--- ElectroGenerate.jl
+++ ElectroGenerate.jl
@@ -5,40 +5,65 @@ using .ElectroInference
 
 ############ Inference data
 
+const b_s = Tuple(fill(100,10))
+const n_s_l = Tuple(fill(500,10))
+const t = Tuple((2.0.^(1:10))./1024)
+const sample_size = 100
+
 # Start with NoEF
-function Posterior_NoEF(fn::String="electro_data")
-    smc_NoEF = SMCProblem(L_NoEF(), Prior(), Tuple(100:50:550), Tuple(50:50:500), Tuple((2.0.^(1:10))/1024))
-    b, I = sample(smc_NoEF, 100)
+function Posterior_NoEF(
+    fn::String="electro_data";
+    batch_sizes=b_s,
+    n_synthetic_likelihoods=n_s_l,
+    temperatures=t,
+    kwargs...
+)
+    smc_NoEF = SMCProblem(L_NoEF(), Prior(), batch_sizes, n_synthetic_likelihoods, temperatures)
+    b, I = sample(smc_NoEF, sample_size)
     save(b, L_NoEF, fn)
     return b, I
 end
 
 # Set up the intermediate prior based on the NoEF output and evaluated against EF only
-function IntermediatePrior(X, fn::String="electro_data")
+function IntermediatePrior(X, fn::String="electro_data"; kwargs...)
     b = load(fn, L_NoEF, (:v, :EB_on, :EB_off, :D))
     I = isaccepted(b)
     π = Sequential(b[I], X)
     return π
 end
-function SequentialPosterior_EF(X, fn::String="electro_data")
+function SequentialPosterior_EF(
+    X,
+    fn::String="electro_data";
+    batch_sizes=b_s,
+    n_synthetic_likelihoods=n_s_l,
+    temperatures=t,
+    kwargs...
+)
     π_seq = IntermediatePrior(X, fn)
-    smc_seq = SMCProblem(L_EF(), π, Tuple(100:50:550), Tuple(50:50:500), Tuple((2.0.^(1:10))/1024))
-    b, I = sample(smc_seq, 100)
+    smc_seq = SMCProblem(L_EF(), π, batch_sizes, n_synthetic_likelihoods, temperatures)
+    b, I = sample(smc_seq, sample_size)
     save(b, L_EF, fn)
     return b, I
 end
-function AllSequentialInference(fn::String="electro_data")
+function AllSequentialInference(fn::String="electro_data"; kwargs...)
     for X in combination_powerset
-        SequentialPosterior_EF(X, fn)
+        SequentialPosterior_EF(X, fn; kwargs...)
     end
     return nothing
 end
 
 # Set up a joint inference problem, with the (known best, as default) parameter space X
-function Posterior_Joint(X=[1,2,4], fn::String="electro_data")
+function Posterior_Joint(
+    X=[1,2,4],
+    fn::String="electro_data";
+    batch_sizes=b_s,
+    n_synthetic_likelihoods=n_s_l,
+    temperatures=t,
+    kwargs...
+)
     π = Prior(X)
-    smc_Joint = SMCProblem(L_Joint(), π, Tuple(100:50:550), Tuple(50:50:500), Tuple((2.0.^(1:10))/1024))
-    b, I = sample(smc_Joint, 100)
+    smc_Joint = SMCProblem(L_Joint(), π, batch_sizes, n_synthetic_likelihoods, temperatures)
+    b, I = sample(smc_Joint, sample_size)
     save(b, L_Joint, fn)
 end
 
