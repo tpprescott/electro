475800235dc04832b8073e706557c93a45093174
TPPrescott <tpprescott@gmail.com>
2021-03-19 14:50:17 +0000

    Upload additional data

--- observations.jl
+++ observations.jl
@@ -1,15 +1,18 @@
+export observation_filter
 
-function observation_filter(sol::DESolution)
-    y = zeros(ComplexF64, 37)
+function observation_filter(sol::DESolution; ninterval=37)
+    y = zeros(ComplexF64, ninterval)
     observation_filter(sol, y)
 end
 function observation_filter(sol::DESolution, y::AbstractVector{ComplexF64})
-    broadcast!(j -> sol(5*j)[2], y, 0:1:36)
+    for j in eachindex(y)
+        y[j] = sol(5*(j-1))[2]
+    end
     return y
 end
 
-function observation_filter(sol::EnsembleSolution)
-    y = zeros(ComplexF64, 37, length(sol))
+function observation_filter(sol::EnsembleSolution; ninterval=37)
+    y = zeros(ComplexF64, ninterval, length(sol))
     observation_filter(sol, y)
 end
 function observation_filter(sol::EnsembleSolution, y::AbstractMatrix{ComplexF64})
@@ -17,14 +20,18 @@ function observation_filter(sol::EnsembleSolution, y::AbstractMatrix{ComplexF64}
     return y
 end
 
-function observation_filter(df::DataFrame)
+function observation_filter(df::DataFrame; ninterval=37)
     z = complex.(df[:x], df[:y])
-    return reshape(z, 37, 50)
+    return reshape(z, ninterval, :)
 end
 
-function observation_filter(csv::CSV.File)
+function observation_filter(csv::CSV.File; ninterval=37)
     z = complex.(csv.x, csv.y)
-    return reshape(z, 37, 50)
+    zmat = reshape(z, ninterval, :)
+    for j in axes(zmat,2)
+        selectdim(zmat, 2, j) .-= zmat[1,j]
+    end
+    return zmat
 end
 
 export pre_step_mean, post_step_mean
