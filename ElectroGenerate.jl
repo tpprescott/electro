517e2f6bdedee64f5fe9a548bf9fce73ce141128
TPPrescott <tpprescott@gmail.com>
2021-04-15 17:11:01 +0100

    Traditional bug squashed

--- ElectroGenerate.jl
+++ ElectroGenerate.jl
@@ -42,7 +42,7 @@ function Posterior_EF(X; kwargs...)
     dataCtrl_list = (yobs_Ctrl_1, yobs_Ctrl_2, yobs_Ctrl)
     data200_list = (yobs_200_1, yobs_200_2, yobs_200)
     
-    SL_list = (L_Joint(data_NoEF=a, data_EF=b) for (a,b) in zip(data_Ctrl_list, data_200_list))
+    SL_list = (L_Joint(data_NoEF=a, data_EF=b) for (a,b) in zip(dataCtrl_list, data200_list))
     fn_list = ("replicate_1", "replicate_2", "merged_data")
     
     for (L, fn) in zip(SL_list, fn_list)
