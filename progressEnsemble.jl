ce2c7627aa1d7f8916ab4aaf40146053301999e0
TPPrescott <tpprescott@gmail.com>
2021-05-04 19:06:43 +0100

    Latest DifferentialEquations version requires SciMLBase not DiffEqBase

--- progressEnsemble.jl
+++ progressEnsemble.jl
@@ -1,6 +1,6 @@
-function DiffEqBase.__solve(prob::DiffEqBase.AbstractEnsembleProblem,
-                 alg::Union{DiffEqBase.DEAlgorithm,Nothing},
-                 ensemblealg::DiffEqBase.BasicEnsembleAlgorithm;
+function SciMLBase.__solve(prob::SciMLBase.AbstractEnsembleProblem,
+                 alg::Union{SciMLBase.DEAlgorithm,Nothing},
+                 ensemblealg::SciMLBase.BasicEnsembleAlgorithm;
                  trajectories, batch_size = trajectories,
                  pmap_batch_size = batch_size÷100 > 0 ? batch_size÷100 : 1, kwargs...)
 
@@ -8,17 +8,17 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractEnsembleProblem,
   num_batches < 1 && error("trajectories ÷ batch_size cannot be less than 1, got $num_batches")
   num_batches * batch_size != trajectories && (num_batches += 1)
 
-  if num_batches == 1 && prob.reduction === DiffEqBase.DEFAULT_REDUCTION
-    elapsed_time = @elapsed u = DiffEqBase.solve_batch(prob,alg,ensemblealg,1:trajectories,pmap_batch_size;kwargs...)
-    _u = DiffEqBase.tighten_container_eltype(u)
-    return DiffEqBase.EnsembleSolution(_u,elapsed_time,true)
+  if num_batches == 1 && prob.reduction === SciMLBase.DEFAULT_REDUCTION
+    elapsed_time = @elapsed u = SciMLBase.solve_batch(prob,alg,ensemblealg,1:trajectories,pmap_batch_size;kwargs...)
+    _u = SciMLBase.tighten_container_eltype(u)
+    return SciMLBase.EnsembleSolution(_u,elapsed_time,true)
   end
 
   converged::Bool = false
   i = 1
   II = (batch_size*(i-1)+1):batch_size*i
 
-  batch_data = DiffEqBase.solve_batch(prob,alg,ensemblealg,II,pmap_batch_size;kwargs...)
+  batch_data = SciMLBase.solve_batch(prob,alg,ensemblealg,II,pmap_batch_size;kwargs...)
 
   u = prob.u_init === nothing ? similar(batch_data, 0) : prob.u_init
   u,converged = prob.reduction(u,batch_data,II)
@@ -29,12 +29,12 @@ function DiffEqBase.__solve(prob::DiffEqBase.AbstractEnsembleProblem,
     else
       II = (batch_size*(i-1)+1):batch_size*i
     end
-    batch_data = DiffEqBase.solve_batch(prob,alg,ensemblealg,II,pmap_batch_size;kwargs...)
+    batch_data = SciMLBase.solve_batch(prob,alg,ensemblealg,II,pmap_batch_size;kwargs...)
     u,converged = prob.reduction(u,batch_data,II)
   end
 
-  _u = DiffEqBase.tighten_container_eltype(u)
+  _u = SciMLBase.tighten_container_eltype(u)
 
-  return DiffEqBase.EnsembleSolution(_u,elapsed_time,converged)
+  return SciMLBase.EnsembleSolution(_u,elapsed_time,converged)
 
 end
